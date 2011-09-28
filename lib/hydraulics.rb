cf423ce42ad176cf13a79907630b7cd21798021d
Andrew Curley <andrew.curley@gmail.com>
2011-09-28 19:17:55 -0400

    Added self.root and self.models_dir to engine.rb for easier access in app

--- lib/hydraulics.rb
+++ lib/hydraulics.rb
@@ -2,4 +2,12 @@ module Hydraulics
   require 'hydraulics/engine' if defined?(Rails)
   require 'hydraulics/custom_validators'
   ActiveRecord::Base.send(:include, CustomValidators)
+
+  def self.root
+    File.expand_path(File.dirname(File.dirname(__FILE__)))
+  end
+
+  def self.models_dir
+    "#{root}/app/models"
+  end
 end
