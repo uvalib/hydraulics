ae3d26792cbe4fad7b84728bb09d621b2fe2c7c7
Andrew Curley <andrew.curley@gmail.com>
2011-12-03 12:06:48 -0500

    Refactor generator

--- lib/generators/hydraulics/hydraulics_generator.rb
+++ lib/generators/hydraulics/hydraulics_generator.rb
@@ -1,25 +1,21 @@
-module Hydraulics
-  module Generators
-    class InstallGenerator < Rails::Generators::Base
-      desc "Installs Hydraulics and generates the necessary migrations."
-      
-      include Rails::Generators::Migration
+class HydraulicsGenerator < Rails::Generators::Base  
+  include Rails::Generators::Migration
 
-      def self.source_root
-        File.expand_path("../templates", __FILE__)
-      end  
+  desc "Installs Hydraulics and generates the necessary migrations."
 
-      def self.next_migration_number(dirname)
-        Time.now.strftime("%Y%m%d%H%M%S")
-      end
+  def self.source_root
+    File.expand_path("../templates", __FILE__)
+  end  
 
-      def create_migrations
-        Dir["#{self.class.source_root}/migrations/*.rb"].sort.each do |filepath|
-          name = File.basename(filepath)
-          migration_template "migrations/#{name}", "db/migrate/#{name.gsub(/^\d+_/,'')}"
-          sleep 1
-        end
-      end
+  def self.next_migration_number(dirname)
+    Time.now.strftime("%Y%m%d%H%M%S")
+  end
+
+  def create_migrations
+    Dir["#{self.class.source_root}/migrations/*.rb"].sort.each do |filepath|
+      name = File.basename(filepath)
+      migration_template "migrations/#{name}", "db/migrate/#{name.gsub(/^\d+_/,'')}"
+      sleep 1
     end
   end
 end
