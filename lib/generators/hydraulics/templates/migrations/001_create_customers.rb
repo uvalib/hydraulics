62e0094b2af5f79b846ff192247261c85d0c392a
Andrew Curley <andrew.curley@gmail.com>
2011-12-16 23:57:07 -0500

    Amend Customers migration

--- lib/generators/hydraulics/templates/migrations/001_create_customers.rb
+++ lib/generators/hydraulics/templates/migrations/001_create_customers.rb
@@ -1,7 +1,9 @@
 class CreateCustomers < ActiveRecord::Migration
   def change
     create_table :customers do |t|
-      t.integer :heard_about_service_id
+      t.references :heard_about_services
+      t.references :departments
+      t.references :academic_statuses
       t.string :last_name
       t.string :first_name
       t.string :address_1
@@ -16,9 +18,16 @@ class CreateCustomers < ActiveRecord::Migration
       t.integer :orders_count, :default => 0
       t.timestamps
     end
+    
     add_index :customers, :last_name
     add_index :customers, :first_name
     add_index :customers, :email
+    add_index :customers, :academic_status_id
+    add_index :customers, :department_id
     add_index :customers, :heard_about_service_id
+
+    add_foreign_key :customers, :academic_statuses
+    add_foreign_key :customers, :departments
+    add_foreign_key :customers, :heard_about_services
   end
 end
