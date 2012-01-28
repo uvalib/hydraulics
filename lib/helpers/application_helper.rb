19e13e640160544ff9f3544aa5d307c115270891
Andrew Curley <andrew.curley@gmail.com>
2012-01-28 00:08:47 -0500

    Removed truncate_words method from ApplicationHelper

--- lib/helpers/application_helper.rb
+++ lib/helpers/application_helper.rb
@@ -1,13 +1,3 @@
 module ApplicationHelper
 
-  # Truncates input string (to number of words specified, using word
-  # boundaries) and returns resulting string.
-  def truncate_words(text, length = 30, end_string = ' ...')
-    if text.nil?
-      return ''
-    else
-      words = text.split()
-      return words[0..(length-1)].join(' ') + (words.length > length ? end_string : '')
-    end
-  end
 end
