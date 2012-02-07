3f13f4dfbd83224f42a92d59fd10e39bc00a0a2a
Andrew Curley <andrew.curley@gmail.com>
2012-02-07 11:51:25 -0500

    Inital commit of UnitImportSource

---
+++ app/models/unit_import_source.rb
@@ -0,0 +1,40 @@
+class UnitImportSource < ActiveRecord::Base
+
+  #------------------------------------------------------------------
+  # relationships
+  #------------------------------------------------------------------
+  belongs_to :unit
+ 
+  #------------------------------------------------------------------
+  # validations
+  #------------------------------------------------------------------
+  validates :unit_id, :presence => true
+  validates :unit, :presence => {
+            :if => 'self.unit_id', 
+            :message => "association with this Unit is no longer valid because the Unit object no longer exists."
+            }
+
+  #------------------------------------------------------------------
+  # callbacks
+  #------------------------------------------------------------------
+
+  #------------------------------------------------------------------
+  # scopes
+  #------------------------------------------------------------------  
+ 
+  #------------------------------------------------------------------
+  # public class methods
+  #------------------------------------------------------------------
+
+  # Returns a string containing a brief, general description of this
+  # class/model.
+  def UnitImportSource.class_description
+    return "Unit Import Source contains the source document used to import unit-related data into the system."
+  end
+
+  #------------------------------------------------------------------
+  # public instance methods
+  #------------------------------------------------------------------
+
+
+end
