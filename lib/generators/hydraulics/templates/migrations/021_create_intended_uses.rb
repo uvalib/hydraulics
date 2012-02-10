f8c05ac0c6bfcfba7588236fd8468e688aafb001
Andrew Curley <andrew.curley@gmail.com>
2012-02-10 18:44:29 -0500

    Change some boolean field params and unique index on IntendedUse migration

--- lib/generators/hydraulics/templates/migrations/021_create_intended_uses.rb
+++ lib/generators/hydraulics/templates/migrations/021_create_intended_uses.rb
@@ -2,10 +2,12 @@ class CreateIntendedUses < ActiveRecord::Migration
   def change
     create_table :intended_uses do |t|
       t.string :description
-      t.boolean :is_internal_use_only
-      t.boolean :is_approved
+      t.boolean :is_internal_use_only, :default => false, :null => false
+      t.boolean :is_approved, :default => false, :null => false
       t.timestamps
     end
+
+    add_index :intended_uses, :description, :unique => true
 
     add_foreign_key :units, :intended_uses
   end
