078f9fa3e13f9613a91fc50473c82761ba05b646
Andrew Curley <andrew.curley@gmail.com>
2012-02-10 16:03:06 -0500

    Initial commit of DvdDeliveryLocation migration

---
+++ lib/generators/hydraulics/templates/migrations/026_create_dvd_delivery_locations.rb
@@ -0,0 +1,13 @@
+class CreateDvdDeliveryLocations < ActiveRecord::Migration
+
+  def change
+    create_table :dvd_delivery_locations do |t|
+      t.string :name
+      t.text :email_desc
+      t.timestamps
+    end
+
+    add_index :dvd_delivery_locations, :name
+    add_foreign_key :orders, :dvd_delivery_locations
+  end
+end
