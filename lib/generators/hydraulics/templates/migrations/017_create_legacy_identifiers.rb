d6a1bd3e8ed2e57947647b721f2d1d5715a5affd
Andrew Curley <andrew.curley@gmail.com>
2012-02-10 11:41:42 -0500

    Create fields and join tables related to LegacyIdentifier migration

--- lib/generators/hydraulics/templates/migrations/017_create_legacy_identifiers.rb
+++ lib/generators/hydraulics/templates/migrations/017_create_legacy_identifiers.rb
@@ -1,8 +1,24 @@
 class CreateLegacyIdentifiers < ActiveRecord::Migration
   def change
     create_table :legacy_identifiers do |t|
-
+      t.string :label
+      t.string :description
+      t.string :legacy_identifier
       t.timestamps
+    end
+
+    add_index :legacy_identifiers, :label
+    add_index :legacy_identifiers, :legacy_identifier
+
+    create_table :bibls_legacy_identifiers, :id => false do |t|
+      t.references :bibls, :legacy_identifiers
+    end
+
+    add_index :bibls_legacy_identifiers, :bibl_id
+    add_index :bibls_legacy_identifiers, :legacy_identifier_id
+
+    create_table :legacy_identifiers_master_files, :id => false do |t|
+      t.references :legacy_identifiers, :master_files
     end
   end
 end
