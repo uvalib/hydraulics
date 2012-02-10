251a4a841e2d47ebd3a9d96b032d6058ccf372f8
Andrew Curley <andrew.curley@gmail.com>
2012-02-10 12:09:20 -0500

    Added foreign keys for LegacyIdentifiersMasterFiles

--- lib/generators/hydraulics/templates/migrations/017_create_legacy_identifiers.rb
+++ lib/generators/hydraulics/templates/migrations/017_create_legacy_identifiers.rb
@@ -17,11 +17,17 @@ class CreateLegacyIdentifiers < ActiveRecord::Migration
     add_index :bibls_legacy_identifiers, :bibl_id
     add_index :bibls_legacy_identifiers, :legacy_identifier_id
 
+    add_foreign_key :bibls_legacy_identifiers, :bibls
+    add_foreign_key :bibls_legacy_identifiers, :legacy_identifiers
+
     create_table :legacy_identifiers_master_files, :id => false do |t|
       t.references :legacy_identifier, :master_file
     end
 
     add_index :legacy_identifiers_master_files, :legacy_identifier_id
     add_index :legacy_identifiers_master_files, :master_file_id
+
+    add_foreign_key :legacy_identifiers_master_files, :legacy_identifiers
+    add_foreign_key :legacy_identifiers_master_files, :master_files
   end
 end
