82becfc83b825d7cbae62c9d79a27b99e5c09489
Andrew Curley <andrew.curley@gmail.com>
2012-02-10 16:41:40 -0500

    Initial commit of UseRights migration

---
+++ lib/generators/hydraulics/templates/migrations/029_create_use_rights.rb
@@ -0,0 +1,14 @@
+class CreateUseRights < ActiveRecord::Migration
+
+  def change
+    create_table :use_rights do |t|
+      t.string :name
+      t.string :description
+      t.timestamps
+    end
+
+    add_index :use_rights, :name
+
+    add_foreign_key :units, :use_rights
+  end
+end
