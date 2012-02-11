57ada5baf64ac647218aa94bb94417d9dab4330a
Andrew Curley <andrew.curley@gmail.com>
2012-02-10 19:50:31 -0500

    Add foreign key for MasterFile and UseRights

--- lib/generators/hydraulics/templates/migrations/029_create_use_rights.rb
+++ lib/generators/hydraulics/templates/migrations/029_create_use_rights.rb
@@ -9,6 +9,7 @@ class CreateUseRights < ActiveRecord::Migration
 
     add_index :use_rights, :name, :unique => true
 
+    add_foreign_key :master_files, :use_rights
     add_foreign_key :units, :use_rights
   end
 end
