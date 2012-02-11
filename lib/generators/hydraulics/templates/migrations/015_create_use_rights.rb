8161b928c4c7b9f85369f7051d979c795146bd5a
Andrew Curley <andrew.curley@gmail.com>
2012-02-10 22:54:04 -0500

    Moved CreateUseRights in migration order

---
+++ lib/generators/hydraulics/templates/migrations/015_create_use_rights.rb
@@ -0,0 +1,15 @@
+class CreateUseRights < ActiveRecord::Migration
+
+  def change
+    create_table :use_rights do |t|
+      t.string :name
+      t.string :description
+      t.timestamps
+    end
+
+    add_index :use_rights, :name, :unique => true
+
+    add_foreign_key :master_files, :use_rights
+    add_foreign_key :units, :use_rights
+  end
+end
