10d9de1a86956e163f497d19dc0782cd77198ee8
Andrew Curley <andrew.curley@gmail.com>
2012-02-14 12:15:29 -0500

    Added relationship to Department model

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,18 +1,8 @@
-# == Schema Information
-#
-# Table name: departments
-#
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  created_at :datetime
-#  updated_at :datetime
-#
-
 class Department < ActiveRecord::Base
-
   #------------------------------------------------------------------
   # relationships
   #------------------------------------------------------------------
+  has_many :customers
  
   #------------------------------------------------------------------
   # validations
