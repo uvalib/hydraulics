37d1c16955869f7d232eaa947bcd95bea3252f61
Andrew Curley <andrew.curley@gmail.com>
2012-02-23 19:32:03 -0500

    Validations and relationships for Component

--- app/models/component.rb
+++ app/models/component.rb
@@ -3,13 +3,27 @@ class Component < ActiveRecord::Base
   # relationships
   #------------------------------------------------------------------
   belongs_to :availability_policy, :counter_cache => true
+  belongs_to :component_type
   belongs_to :indexing_scenario, :counter_cache => true
+  belongs_to :use_rights, :counter_cache => true
   
-  has_many :master_file
+  has_many :automation_messages
+  has_many :master_files
+  has_many :image_master_files, :class_name => 'MasterFile', :conditions => "tech_meta_type = 'image'"
+
+  has_and_belongs_to_many :bibls
+  has_and_belongs_to_many :containers
  
   #------------------------------------------------------------------
   # validations
   #------------------------------------------------------------------
+  validates :component_type, :bibl_id, :presence => true
+  validates :component_type, :presence => {
+    :message => 'association with this ComponentType is no longer valid.'
+  }
+  validates :bibl, :presence => {
+    :message => 'associate with this Bibl is no longer valid.'
+  }
  
   #------------------------------------------------------------------
   # callbacks
