80bad5af53ed712acf3178bb8a47655ae8a94ecb
Andrew Curley <andrew.curley@gmail.com>
2012-03-02 23:47:52 -0500

    Renamed BillingAddress migration to Addresss

---
+++ lib/generators/hydraulics/templates/migrations/014_create_addresses.rb
@@ -0,0 +1,18 @@
+class CreateAddress < ActiveRecord::Migration
+
+  def change
+    create_table :addresses do |t|
+      t.string :last_name
+      t.string :first_name
+      t.string :address_1
+      t.string :address_2
+      t.string :city
+      t.string :state
+      t.string :country
+      t.string :post_code
+      t.string :phone
+      t.string :organization
+      t.timestamps
+    end
+  end 
+end
