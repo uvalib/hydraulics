1aee6feebaadba422e43be3b2e19a0c9cb01c017
Andrew Curley <andrew.curley@gmail.com>
2012-03-08 12:23:53 -0500

    Add polymorphic fields to Address migration

--- lib/generators/hydraulics/templates/migrations/014_create_addresses.rb
+++ lib/generators/hydraulics/templates/migrations/014_create_addresses.rb
@@ -1,6 +1,11 @@
 class CreateAddresses < ActiveRecord::Migration
   def change
     create_table :addresses do |t|
+      # Custom polymorphism
+      t.integer :addressable_id, :null => false
+      t.string :addressable_type, :null => false, :limit => 20
+      t.string :address_type, :null => false, :limit => 20
+
       t.string :last_name
       t.string :first_name
       t.string :address_1
