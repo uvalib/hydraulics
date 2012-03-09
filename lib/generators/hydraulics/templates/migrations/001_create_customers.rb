c1cc6fe41d6349e62585b986ec214c208a1e5d6b
Andrew Curley <andrew.curley@gmail.com>
2012-03-09 17:47:04 -0500

    Remove unnecessary fields from Customer migration

--- lib/generators/hydraulics/templates/migrations/001_create_customers.rb
+++ lib/generators/hydraulics/templates/migrations/001_create_customers.rb
@@ -4,13 +4,6 @@ class CreateCustomers < ActiveRecord::Migration
       t.references :department, :academic_status, :heard_about_service # non-required references
       t.string :last_name
       t.string :first_name
-      t.string :address_1
-      t.string :address_2
-      t.string :city
-      t.string :state
-      t.string :country
-      t.string :post_code
-      t.string :phone
       t.string :email
       t.string :organization
       t.integer :orders_count, :default => 0
