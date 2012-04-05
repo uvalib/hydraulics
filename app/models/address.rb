de6e0ab209eb0c97db687de98513ccac87148dc7
Andrew Curley <andrew.curley@gmail.com>
2012-04-05 11:20:16 -0400

    Change Address validations to allow_blank

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,8 +3,8 @@ class Address < ActiveRecord::Base
 
   # validates :customer_id, :agency_id, :uniqueness => true
   validates :address_1, :country, :city, :presence => true
-  validates :last_name, :first_name, :person_name_format => true, :allow_nil => true
-  validates :city, :city_format => true, :allow_nil => true
-  validates :phone, :phone_format => true, :allow_nil => true
+  validates :last_name, :first_name, :person_name_format => true, :allow_blank => true
+  validates :city, :city_format => true, :allow_blank => true
+  validates :phone, :phone_format => true, :allow_blank => true
 
 end
