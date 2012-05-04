cce87070e847a948327417509e36393a9ccb1fc9
Andrew Curley <andrew.curley@gmail.com>
2012-05-04 13:56:57 -0400

    Add AcademicStatus has_may Requests

--- app/models/academic_status.rb
+++ app/models/academic_status.rb
@@ -4,6 +4,7 @@ class AcademicStatus < ActiveRecord::Base
   #------------------------------------------------------------------
   has_many :customers
   has_many :orders, :through => :customers
+  has_many :requests, :through => :customers, :conditions => ['orders.order_status = ?', 'requested']
   has_many :units, :through => :orders
   has_many :master_files, :through => :units
  
