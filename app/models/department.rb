9b23e0cfa13e3cf97f590d55e4ac861b27b8465e
Andrew Curley <andrew.curley@gmail.com>
2012-05-04 14:02:19 -0400

    Add multiple relationships for Department

--- app/models/department.rb
+++ app/models/department.rb
@@ -3,6 +3,10 @@ class Department < ActiveRecord::Base
   # relationships
   #------------------------------------------------------------------
   has_many :customers
+  has_many :requests, :through => :customers, :conditions => ['orders.order_status = ?', 'requested']
+  has_many :orders, :through => :customers
+  has_many :units, :through => :orders
+  has_many :master_files, :through => :units
  
   #------------------------------------------------------------------
   # validations
