ad4eb354a006e50be38e9d545cc47eb4eb221d69
Andrew Curley <andrew.curley@gmail.com>
2012-06-14 10:12:55 -0400

    Change Address.address_1 validation message

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,7 +2,9 @@ class Address < ActiveRecord::Base
   belongs_to :addressable, :polymorphic => true
 
   # validates :customer_id, :agency_id, :uniqueness => true
-  validates :address_1, :country, :city, :presence => true
+  validates :address_1, :country, :city, :presence => {
+    :message => 'is required.'
+  }
   validates :last_name, :first_name, :person_name_format => true, :allow_blank => true
   validates :city, :city_format => true, :allow_blank => true
   validates :phone, :phone_format => true, :allow_blank => true
