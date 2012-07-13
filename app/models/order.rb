3d8cb73162d49f85ef8e8d39f3e37a4b4030a6f0
Andrew Curley <andrew.curley@gmail.com>
2012-07-13 17:13:02 -0400

    Add before_save :set_blank_values_to_nil for Order and remove previous callback that only operated on email

--- app/models/order.rb
+++ app/models/order.rb
@@ -102,8 +102,8 @@ class Order < ActiveRecord::Base
     # boolean fields cannot be NULL at database level
     self.is_approved = 0 if self.is_approved.nil? 
     self.is_approved = 1 if self.order_status == 'approved'
-    self.email = nil if self.email.blank?
   end
+  before_save :set_blank_values_to_nil
   
   #------------------------------------------------------------------
   # scopes
