a8f125cb84b2a8f3ae2c654b59a8253e9ec6b534
Andrew Curley <andrew.curley@gmail.com>
2013-04-16 18:57:01 -0400

    Remove destroyable code on Agency model

--- app/models/agency.rb
+++ app/models/agency.rb
@@ -40,21 +40,6 @@ class Agency < ActiveRecord::Base
     return 'Agency represents a project or organization associated with an Order.'
   end
   
-  #------------------------------------------------------------------
-  # public instance methods
-  #------------------------------------------------------------------
-  # Returns a boolean value indicating whether it is safe to delete this
-  # Customer from the database. Returns +false+ if this record has dependent
-  # records in other tables, namely associated Order records. (We do not check
-  # for a BillingAddress record, because it is considered merely an extension of
-  # the Customer record; it gets destroyed when the Customer is destroyed.)
-  #
-  # This method is public but is also called as a +before_destroy+ callback.
-  # def destroyable?  
-  def destroyable?
-    ! orders? || ! requests?
-  end
-  
   # Returns a boolean value indicating whether this Customer has
   # associated Order records.
   def orders?
