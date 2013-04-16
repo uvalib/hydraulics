822acfcf21c8893bfb8ed15ed6b403ed69830fda
Andrew Curley <andrew.curley@gmail.com>
2013-04-16 16:28:28 -0400

    Ammend the PhoneFormatValidator

--- lib/hydraulics/custom_validators.rb
+++ lib/hydraulics/custom_validators.rb
@@ -37,7 +37,7 @@ module Hydraulics
 
     class PhoneFormatValidator < ActiveModel::EachValidator
       def validate_each(record, attribute, value)
-        if not value =~ /^([0-9\.+ -]+)$/i
+        if not value =~ /^([0-9\.+ -\\(\\)]+)$/i
           record.errors.add(attribute, "should only contain digits, period, plus sign, hyphen, and spaces")
         end
       end
