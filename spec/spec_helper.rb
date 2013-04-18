6edd25a6d227fdfbcdb7783b6dc8324b20d163a0
Andrew Curley <andrew.curley@gmail.com>
2013-04-18 02:46:45 -0400

    Only generage coverage html when running in Travis CI

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,7 @@
-require 'coveralls'
-Coveralls.wear!('rails')
+if ENV['TRAVIS']
+  require 'coveralls'
+  Coveralls.wear!('rails')
+end
 
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path("../dummy/config/environment.rb",  __FILE__)
