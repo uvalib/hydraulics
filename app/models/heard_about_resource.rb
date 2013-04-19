d4f13c487fc79cdf28399f6b4ae25785fbaa1aff
Andrew Curley <andrew.curley@gmail.com>
2013-04-18 22:08:15 -0400

    Simplify and remove duplicative code from two models

--- app/models/heard_about_resource.rb
+++ app/models/heard_about_resource.rb
@@ -1,39 +1,11 @@
 class HeardAboutResource < ActiveRecord::Base
-
-  #------------------------------------------------------------------
-  # relationships
-  #------------------------------------------------------------------
   has_many :units
   has_many :master_files, :through => :units
   has_many :orders, :through => :units
   has_many :customers, :through => :orders
- 
-  #------------------------------------------------------------------
-  # validations
-  #------------------------------------------------------------------
- 
-  #------------------------------------------------------------------
-  # callbacks
-  #------------------------------------------------------------------
-  before_save do
-    self.is_approved = 0 if self.is_approved.nil?
-    self.is_internal_use_only = 0 if self.is_internal_use_only.nil?
-  end
-  
-  #------------------------------------------------------------------
-  # scopes
-  #------------------------------------------------------------------
+
   scope :approved, where(:is_approved => true)
   scope :not_approved, where(:is_approved => false)
   scope :internal_use_only, where(:is_internal_use_only => true)
   scope :publicly_available, where(:is_internal_use_only => false)
- 
-  #------------------------------------------------------------------
-  # public class methods
-  #------------------------------------------------------------------
- 
-  #------------------------------------------------------------------
-  # public instance methods
-  #------------------------------------------------------------------
-
 end
--- app/models/heard_about_service.rb
+++ app/models/heard_about_service.rb
@@ -1,46 +1,14 @@
 class HeardAboutService < ActiveRecord::Base
-
-  #------------------------------------------------------------------
-  # relationships
-  #------------------------------------------------------------------
   has_many :customers
   has_many :orders, :through => :customers
   has_many :units, :through => :orders
   has_many :master_files, :through => :units
 
-  #------------------------------------------------------------------
-  # validations
-  #------------------------------------------------------------------
- 
-  #------------------------------------------------------------------
-  # callbacks
-  #------------------------------------------------------------------
-  before_save do
-    self.is_approved = 0 if self.is_approved.nil?
-    self.is_internal_use_only = 0 if self.is_internal_use_only.nil?
-  end
-
-  #------------------------------------------------------------------
-  # scopes
-  #------------------------------------------------------------------  
   scope :approved, where(:is_approved => true)
   scope :not_approved, where(:is_approved => false)
   scope :internal_use_only, where(:is_internal_use_only => true)
   scope :publicly_available, where(:is_internal_use_only => false)
 
-  #------------------------------------------------------------------
-  # aliases
-  #------------------------------------------------------------------
   # Necessary for Active Admin to poplulate pulldown menu
   alias_attribute :name, :description
- 
-  #------------------------------------------------------------------
-  # public class methods
-  #------------------------------------------------------------------
- 
-  #------------------------------------------------------------------
-  # public instance methods
-  #------------------------------------------------------------------
-
 end
-
