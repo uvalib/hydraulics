09b5bdcb74fe8d04f0eab0a78b079df23952e36b
Andrew Curley <andrew.curley@gmail.com>
2013-04-19 10:57:09 -0400

    Added new unit test for Order

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
 describe Order do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should be return true for active? if the order_status is ['requested', 'approved']" do
+    ['requested', 'approved'].each {|value|
+      FactoryGirl.build(:order, order_status: value).active?.should be_true
+    }
+  end
+
+  it "should be return false for active? if the order_status is ['deferred', 'canceled']" do
+    ['deferred', 'canceled'].each {|value|
+      FactoryGirl.build(:order, order_status: value).active?.should be_false
+    }
+  end
+
+  it "should return false for active? if order_status is anything else" do
+    FactoryGirl.build(:order, order_status: Faker::Lorem.word).active?.should be_false
+  end
 end
