a513ab534693cac807828e649d518150d443784d
Andrew Curley <andrew.curley@gmail.com>
2013-04-22 13:30:58 -0400

    Add new class variable for repo and cleanup code

--- app/concerns/pidable.rb
+++ app/concerns/pidable.rb
@@ -26,126 +26,92 @@ module Pidable
     before_save :assign_pid
   end
 
+  @@repo = Rubydora.connect :url => 'http://localhost:8080/fedora', :user => 'fedoraAdmin', :password => 'fedoraAdmin'
+
   def assign_pid
     self.pid = get_pid if self.pid.nil?
   end
 
   # Query Fedora to get next pid
   def get_pid(pid_namespace = nil)
-    # Determine proper namespace
-    # Rails.env == 'production' ?  pid_namespace = 'uva-lib' : pid_namespace = 'test'
-
-    repo = Rubydora.connect :url => 'http://localhost:8080/fedora', :user => 'fedoraAdmin', :password => 'fedoraAdmin'
-    xml = Nokogiri.XML(repo.next_pid({:namespace => pid_namespace }))
+    xml = Nokogiri.XML(@@repo.next_pid({:namespace => pid_namespace }))
     xml.remove_namespaces!
-    xml.xpath('//pid').first.content
+    xml.xpath('//pid[1]').text
   end
 
   def in_dl?
     date_dl_ingest?
   end
 
-
-
-
-
-
-  # require 'solr'
-
-  # require 'activemessaging/processor'
-  # include ActiveMessaging::MessageSender
-
-  # Returns a boolean indicating whether a datastream for a piddable object exists in the Fedora repo
-  def datastream_exists?(dsID)
-    # Set up REST client
-    resource = RestClient::Resource.new FEDORA_REST_URL, :user => Fedora_username, :password => Fedora_password
-
-    url = "/objects/#{self.pid}/datastreams/#{dsID}?format=xml"
-    begin
-      response = resource[url].get
-    rescue RestClient::ResourceNotFound, RestClient::InternalServerError, RestClient::RequestTimeout
-      return false
-    end
-    return true
-  end
-
-  # Query a given Solr index to determine if object's index record is found there.  A default Solr index is used if none is provided at method invocation.
-  def exists_in_index?(solr_url = nil)
-    solr_url = STAGING_SOLR_URL if solr_url.nil?
-    if pid.nil?
-      return false
-    else
-      @solr_connection = Solr::Connection.new("#{solr_url}", :autocommit => :off)
-
-      begin
-        hits = @solr_connection.query("id:#{pid.gsub(/:/, '?')}").hits
-      rescue RestClient::ResourceNotFound, RestClient::InternalServerError, RestClient::RequestTimeout
-        return false
-      end
-
-      return true unless hits.length != 1
-    end
-  end
-
   # Returns a boolean indicating whether an object with the same PID as this one exists in the Fedora repo
   def exists_in_repo?
-    if pid.nil?
-      return false
-    else
-      # Determine whether this object exists (using Fedora API findObjects, which uses HTTP GET)
-      # * If Fedora finds the object, no exception occurs
-      # * If Fedora can't find the object, RestClient::ResourceNotFound exception occurs
-      # * If any other exception occurs, it remains unhandled and gets raised
-      resource = RestClient::Resource.new FEDORA_REST_URL, :user => Fedora_username, :password => Fedora_password
-      url = "/objects?query=pid%3D#{pid}&resultFormat=xml&pid=true"
-
+    if self.pid?
       begin
-        response = resource[url].get
-      rescue RestClient::ResourceNotFound, RestClient::InternalServerError, RestClient::RequestTimeout
+        @@repo.find("#{self.pid}").is_a?(Rubydora::DigitalObject)
+      rescue Rubydora::RecordNotFound
         return false
       end
-      return response.include? "#{pid}"
     end
   end
 
   def remove_from_repo
-    if pid.nil?
-      return false
-    else
-      resource = RestClient::Resource.new FEDORA_REST_URL, :user => Fedora_username, :password => Fedora_password
-      url = "/objects/#{pid}"
-
+    if self.pid?
       begin
-        response = resource[url].delete
-      rescue RestClient::ResourceNotFound, RestClient::InternalServerError, RestClient::RequestTimeout
+        @@repo.purge_object pid: :self.pid
+      rescue Rubydora::RecordNotFound
         return false
       end
-      return response.include? "#{Time.now.strftime('%Y-%m-%d')}"
     end
   end
 
-  def remove_from_index(solr_url = nil)
-    solr_url = STAGING_SOLR_URL if solr_url.nil?
-    if pid.nil?
-      return false
-    else
-      @solr_connection = Solr::Connection.new("#{solr_url}", :autocommit => :off)
 
-      begin
-        @solr_connection.delete(pid)
-      rescue RestClient::ResourceNotFound, RestClient::InternalServerError, RestClient::RequestTimeout
-        return false
-      end
-      return true
-    end
+##########
 
-  end
+  # # Returns a boolean indicating whether a datastream for a piddable object exists in the Fedora repo
+  # def datastream_exists?(dsID)
+  #   url = "/objects/#{self.pid}/datastreams/#{dsID}?format=xml"
+  #   begin
+  #     response = resource[url].get
+  #   rescue RestClient::ResourceNotFound, RestClient::InternalServerError, RestClient::RequestTimeout
+  #     return false
+  #   end
+  #   return true
+  # end
 
-  # Methods for ingest and Fedora management workflows
-  def update_metadata(datastream)
-    message = ActiveSupport::JSON.encode( { :object_class => self.class.to_s, :object_id => self.id, :datastream => datastream })
-    publish :update_fedora_datastreams, message
-    # flash[:notice] = "#{params[:datastream].gsub(/_/, ' ').capitalize} datastream(s) being updated."
-    # redirect_to :action => "show", :controller => "bibl", :id => params[:object_id]
-  end
+  # require 'solr'
+  # # Query a given Solr index to determine if object's index record is found there.  A default Solr index is used if none is provided at method invocation.
+  # def exists_in_index?(solr_url = nil)
+  #   solr_url = STAGING_SOLR_URL if solr_url.nil?
+  #   if pid.nil?
+  #     return false
+  #   else
+  #     @solr_connection = Solr::Connection.new("#{solr_url}", :autocommit => :off)
+
+  #     begin
+  #       hits = @solr_connection.query("id:#{pid.gsub(/:/, '?')}").hits
+  #     rescue RestClient::ResourceNotFound, RestClient::InternalServerError, RestClient::RequestTimeout
+  #       return false
+  #     end
+
+  #     return true unless hits.length != 1
+  #   end
+  # end
+
+
+  # def remove_from_index(solr_url = nil)
+  #   solr_url = STAGING_SOLR_URL if solr_url.nil?
+  #   if pid.nil?
+  #     return false
+  #   else
+  #     @solr_connection = Solr::Connection.new("#{solr_url}", :autocommit => :off)
+
+  #     begin
+  #       @solr_connection.delete(pid)
+  #     rescue RestClient::ResourceNotFound, RestClient::InternalServerError, RestClient::RequestTimeout
+  #       return false
+  #     end
+  #     return true
+  #   end
+
+  # end
 end
